a3f5423adfb11d04f33e9e991dce6440112986ac
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-02-13 10:38:51 +0530

    Add files via upload

---
+++ List_Expresion1.py
@@ -0,0 +1,11 @@
+# PRINT O TH VALUE IN LIST
+List = ['mercet','college','MRCET!']
+print(List[0])
+
+# PRINT 1ST VALUE IN LIST
+List = ['mercet','college','MRCET!']
+print(List[-2])
+
+# PRINT 2ND VALUE IN LIST
+List = ['mercet','collage','MERCET!']
+print(List[1:])
